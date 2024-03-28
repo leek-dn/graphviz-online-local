8842ee48cb660eba1c192db034ed5b7fbf4390b0
leek-dn <55885167+leek-dn@users.noreply.github.com>
2024-03-28 22:36:07 +0100

    renderbrowserpatch works with latest emsdk 3.1.56

--- renderbrowserpatcher.sh
+++ renderbrowserpatcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # a small script to patch the render.browser.js function to allow wasm
 cp src/render.browser.js src/render.browser.js.orig
-sed 's@WebAssembly\.instantiateStreaming||I()||"function"!=typeof fetch?a(e):fetch(O,{credentials:"same-origin"})\.then((function(n){return WebAssembly\.instantiateStreaming(n,i)\.then(e,(function(n){return l("wasm streaming compile failed: "+n),l("falling back to ArrayBuffer instantiation"),a(e)}))}))@WebAssembly.instantiate||I()?a(e):WebAssembly.instantiate(new Uint8Array([55834]),i).then(e,(function(n){return l("wasm streaming compile failed: "+n),l("falling back to ArrayBuffer instantiation"),a(e)}))@' src/render.browser.js.orig > src/render.browser.js
+sed 's@WebAssembly\.instantiateStreaming\(||.*\)||"function"!=typeof fetch?\(.*\):fetch(\(.*\),{credentials:"same-origin"})\.then(.*WebAssembly\.instantiateStreaming(\(.*\),\(.*\))\.then(e,(function(\(.*\)){return \(.*\)(["`]wasm streaming compile failed: \(.*\)),\7("falling back to ArrayBuffer instantiation"),\2}))))@'\
+'WebAssembly.instantiate\1?\2:WebAssembly.instantiate(new Uint8Array([55834]),\5).then(e,(function(n){return l("wasm streaming compile failed: "+n),l("falling back to ArrayBuffer instantiation"),\2}))@' src/render.browser.js.orig > src/render.browser.js
 rm src/render.browser.js.orig
